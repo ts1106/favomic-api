d798ef308842303f303694934b26adcdeababbbf
TS-1106 <ts.dev.1106@gmail.com>
2022-12-16 12:56:12 +0900

    add router

---
+++ internal/router/comic.go
@@ -0,0 +1,38 @@
+package router
+
+import (
+	"github.com/bufbuild/connect-go"
+	"github.com/go-chi/chi/v5"
+	"github.com/ts1106/favomic-api/ent"
+	"github.com/ts1106/favomic-api/gen/ent/proto/entpb"
+)
+
+func comicRouter(c *ent.Client) chi.Router {
+	r := chi.NewRouter()
+	svc := entpb.NewComicService(c)
+	r.Handle("/Create", connect.NewUnaryHandler(
+		"/Create",
+		svc.Create,
+	))
+	r.Handle("/Get", connect.NewUnaryHandler(
+		"/Get",
+		svc.Get,
+	))
+	r.Handle("/Update", connect.NewUnaryHandler(
+		"/Update",
+		svc.Update,
+	))
+	r.Handle("/Delete", connect.NewUnaryHandler(
+		"/Delete",
+		svc.Delete,
+	))
+	r.Handle("/List", connect.NewUnaryHandler(
+		"/List",
+		svc.List,
+	))
+	r.Handle("/BatchCreate", connect.NewUnaryHandler(
+		"/BatchCreate",
+		svc.BatchCreate,
+	))
+	return r
+}
